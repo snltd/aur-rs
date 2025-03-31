a4aa01d27f6cfade3d10efe64490db60e0d572ea
snltd <services@id264.net>
2025-03-31 16:33:16 +0000

    replace horrible ls test

--- tests/syncflac.rs
+++ tests/syncflac.rs
@@ -3,7 +3,10 @@ mod test {
     use assert_cmd::Command;
     use assert_fs::prelude::*;
     use aur::test_utils::spec_helper::{fixture, sample_output};
+    use glob::glob;
     use predicates::prelude::*;
+    use std::collections::BTreeSet;
+    use std::path::PathBuf;
 
     #[test]
     #[ignore]
@@ -31,12 +34,66 @@ mod test {
                     .display()
             )));
 
-        Command::new("ls")
-            .current_dir(&dir_under_test)
-            .arg("-R")
-            .assert()
-            .success()
-            .stdout(sample_output("commands/syncflac/ls"));
+        let mut expected_files = BTreeSet::new();
+
+        let files = [
+            "flac",
+            "flac/albums",
+            "flac/albums/abc",
+            "flac/albums/abc/already.synced",
+            "flac/albums/abc/already.synced/01.already.synced.flac",
+            "flac/albums/tuv",
+            "flac/albums/tuv/tester.flac_album",
+            "flac/albums/tuv/tester.flac_album/01.tester.song_1.flac",
+            "flac/albums/tuv/tester.flac_album/02.tester.song_2.flac",
+            "flac/eps",
+            "flac/eps/band.flac_and_mp3_unequal",
+            "flac/eps/band.flac_and_mp3_unequal/01.band.song_1.flac",
+            "flac/eps/band.flac_and_mp3_unequal/02.band.song_2.flac",
+            "flac/eps/band.flac_ep",
+            "flac/eps/band.flac_ep/01.band.song_1.flac",
+            "flac/eps/band.flac_ep/02.band.song_2.flac",
+            "flac/tracks",
+            "flac/tracks/singer.song.flac",
+            "mp3",
+            "mp3/albums",
+            "mp3/albums/abc",
+            "mp3/albums/abc/already.synced",
+            "mp3/albums/abc/already.synced/01.already.synced.mp3",
+            "mp3/albums/abc/artist.mp3_album",
+            "mp3/albums/abc/artist.mp3_album/01.artist.song_1.mp3",
+            "mp3/albums/abc/artist.mp3_album/02.artist.song_1.mp3",
+            "mp3/albums/tuv",
+            "mp3/albums/tuv/tester.flac_album",
+            "mp3/albums/tuv/tester.flac_album/01.tester.song_1.mp3",
+            "mp3/albums/tuv/tester.flac_album/02.tester.song_2.mp3",
+            "mp3/eps",
+            "mp3/eps/band.flac_and_mp3_unequal",
+            "mp3/eps/band.flac_and_mp3_unequal/01.band.song_1.mp3",
+            "mp3/eps/band.flac_and_mp3_unequal/02.band.song_2.mp3",
+            "mp3/eps/band.flac_ep",
+            "mp3/eps/band.flac_ep/01.band.song_1.mp3",
+            "mp3/eps/band.flac_ep/02.band.song_2.mp3",
+            "mp3/eps/band.mp3_ep",
+            "mp3/eps/band.mp3_ep/01.band.song_1.mp3",
+            "mp3/eps/band.mp3_ep/02.band.song_2.mp3",
+            "mp3/tracks",
+            "mp3/tracks/singer.song.mp3",
+            "mp3/tracks/this.that.mp3",
+        ];
+
+        for f in files {
+            expected_files.insert(dir_under_test.join(f));
+        }
+
+        let glob_pattern = format!("{}/**/*", dir_under_test.display());
+
+        let actual_files: BTreeSet<PathBuf> = glob(glob_pattern.as_str())
+            .unwrap()
+            .filter_map(Result::ok)
+            .collect();
+
+        assert_eq!(expected_files, actual_files);
 
         let sample_file = dir_under_test
             .join("mp3/albums/tuv/tester.flac_album")
